dd05d0c3733682fbde8fd0415252fc88013226cd
Sirio Negri <ziriuz84@gmail.com>
2024-07-07 16:57:21 +0200

    first trying

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rocket;
+
+#[rocket::get("/world")]
+fn world() -> &'static str {
+    "Hello World maddafakka!"
+}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build().mount("/hello", rocket::routes![world])
 }
